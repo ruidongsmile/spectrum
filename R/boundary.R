7fdc2dfd70ca8bcc3748fe0d90a36b88e52cdec0
ruidongsmile <ruidongsmile@mail.com>
2024-04-27 21:57:50 +0200

    Create function pers_lap_schur()

--- R/boundary.R
+++ R/boundary.R
@@ -47,6 +47,7 @@ boundary_opr <- function(sp_cmplx, order){
 
 
 pers_lap <- function(cmplx_K, cmplx_L, order = 1, W_q_minus_1_K=NULL, W_q_K=NULL, W_q_plus_1_L=NULL){
+  # Thm 3.1 in Memoli's paper
   # order is the order of simplices
   bd_opr_q_K <- cmplx_K |>
     boundary_opr(order = order)
@@ -79,11 +80,7 @@ pers_lap <- function(cmplx_K, cmplx_L, order = 1, W_q_minus_1_K=NULL, W_q_K=NULL
 
   if(order == 0){
     lap_q_down_K <- 0
-  }
-  # else if(all(bd_opr_q_plus_1_L == 0)){
-  #  lap <- W_q_K_mat %*% t(bd_opr_q_K) %*% diag(1 / W_q_minus_1_K) %*% bd_opr_q_K
-  #}
-  else{
+  } else{
     lap_q_down_K <- W_q_K_mat %*% t(bd_opr_q_K) %*% diag(1 / W_q_minus_1_K) %*% bd_opr_q_K
   }
 
@@ -96,6 +93,8 @@ pers_lap <- function(cmplx_K, cmplx_L, order = 1, W_q_minus_1_K=NULL, W_q_K=NULL
     D_q_plus_1_L <- as.matrix(bd_opr_q_plus_1_L[seq(from=n_q_K + 1, to =n_q_L), ])
 
     gauss_red <- matlib::gaussianElimination(t(D_q_plus_1_L), diag(rep(1, ncol(D_q_plus_1_L))))
+
+    # gauss_red <- pracma::rref(cbind(t(D_q_plus_1_L), diag(rep(1, ncol(D_q_plus_1_L)))))
 
     R_q_plus_1_L <- t(gauss_red[, 1:nrow(D_q_plus_1_L), drop=FALSE])
     Y_mat <- t(gauss_red[, (nrow(D_q_plus_1_L) + 1):ncol(gauss_red), drop=FALSE])
@@ -115,7 +114,67 @@ pers_lap <- function(cmplx_K, cmplx_L, order = 1, W_q_minus_1_K=NULL, W_q_K=NULL
   return(lap)
 }
 
+pers_lap_schur <- function(cmplx_K, cmplx_L, order = 1, W_q_minus_1_K=NULL, W_q_K=NULL, W_q_L=NULL, W_q_plus_1_L=NULL){
+  # a function to compute persistent laplacian using Schur complement
+  bd_opr_q_K <- cmplx_K |>
+    boundary_opr(order = order)
 
+  bd_opr_q_plus_1_L <- cmplx_L |>
+    boundary_opr(order = order + 1)
+
+  lap_q_down_K <- bd_opr_q_K
+
+  n_q_L <- length(cmplx_L[sapply(cmplx_L, length) == order + 1])
+  n_q_K <- length(cmplx_K[sapply(cmplx_K, length) == order + 1])
+
+  n_q_minus_1_K <- length(cmplx_K[sapply(cmplx_K, length) == order])
+  n_q_plus_1_L <- length(cmplx_L[sapply(cmplx_L, length) == order + 2])
+
+  if(is.null(W_q_minus_1_K)){
+    W_q_minus_1_K <- if(n_q_minus_1_K != 0){rep(1, n_q_minus_1_K)} else{1}
+  }
+  W_q_minus_1_K_mat <- diag(W_q_minus_1_K)
+
+  if(is.null(W_q_K)){
+    W_q_K <- if(n_q_K != 0){rep(1, n_q_K)} else{1}
+  }
+  W_q_K_mat <- diag(W_q_K)
+
+
+  if(is.null(W_q_L)){
+    W_q_L <- if(n_q_L !=0){rep(1, n_q_L)}else{1}
+  }
+  W_q_L_mat <- diag(W_q_L)
+
+  if(is.null(W_q_plus_1_L)){
+    W_q_plus_1_L <- if(n_q_plus_1_L !=0){rep(1, n_q_plus_1_L)}else{1}
+  }
+  W_q_plus_1_L_mat <- diag(W_q_plus_1_L)
+
+  if(order == 0){
+    lap_q_down_K <- 0
+  } else{
+    lap_q_down_K <- W_q_K_mat %*% t(bd_opr_q_K) %*% diag(1 / W_q_minus_1_K) %*% bd_opr_q_K
+  }
+
+  lap_q_up_L <- bd_opr_q_plus_1_L %*% W_q_plus_1_L_mat %*% t(bd_opr_q_plus_1_L) %*% diag(1 / W_q_L)
+
+  lap_q_down_K <- W_q_K_mat %*% t(bd_opr_q_K) %*% diag(1 / W_q_minus_1_K) %*% bd_opr_q_K
+
+  if (n_q_K < n_q_L){
+    idx_K_L <- seq(from=n_q_K + 1, to =n_q_L)
+    lap_q_up_L_sub <- lap_q_up_L[idx_K_L, idx_K_L]
+
+    lap_q_up_K_L <- lap_q_up_L[1:n_q_K, 1:n_q_K] - lap_q_up_L[1:n_q_K, idx_K_L] %*% MASS::ginv(lap_q_up_L_sub) %*% lap_q_up_L[idx_K_L, 1:n_q_K]
+
+    lap <- lap_q_up_K_L + lap_q_down_K
+  } else if (n_q_K == n_q_L){
+    lap <- lap_q_up_L + lap_q_down_K
+  }
+
+  return(lap)
+
+}
 
 
 
